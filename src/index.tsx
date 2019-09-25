ccd31d701aff9c8e1f2f8f7e35a0b0ec6e63a2a0
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2019-09-25 12:50:08 +0900

    [update]

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 
 export default function App(): JSX.Element {
-  const sum = (a: number, b: number) => a + b
-  return <h1>Hello {sum(15, 15)}</h1>
+  return (
+    <Fragment>
+      <h1>Todo List</h1>
+      <form>
+        <input type="text" required />
+        <button type="submit">Add Todo</button>
+      </form>
+    </Fragment>
+  )
 }
 
 const root = document.getElementById('root')
